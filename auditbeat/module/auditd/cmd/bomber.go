4fc9c3ee4e7cb2df1daf901bcd0e11b3e204ca40
Adrian Serrano <adrisr83@gmail.com>
2021-05-28 17:11:10 +0200

    usable bomb

--- auditbeat/module/auditd/cmd/bomber.go
+++ auditbeat/module/auditd/cmd/bomber.go
@@ -3,15 +3,25 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"syscall"
 
 	"github.com/elastic/go-libaudit/v2"
 )
 
 func main() {
+	count := -1
+	if len(os.Args) > 1 {
+		var err error
+		if count, err = strconv.Atoi(os.Args[1]); err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to parse count argument '%s': %v\n", os.Args[1], err)
+			os.Exit(3)
+		}
+	}
 	cli, err := libaudit.NewAuditClient(os.Stdout)
 	if err != nil {
-		panic(err)
+		fmt.Fprintf(os.Stderr, "Connect to audit failed: %v\n", err)
+		os.Exit(1)
 	}
 	msg := syscall.NetlinkMessage{
 		Header: syscall.NlMsghdr{
@@ -20,9 +30,13 @@ func main() {
 		},
 		Data: []byte("XXX HELLO WORLD! XXX"),
 	}
-	for {
+	for count == -1 || count > 0 {
 		if _, err := cli.Netlink.Send(msg); err != nil {
 			fmt.Fprintf(os.Stderr, "Write error: %v", err)
+			os.Exit(2)
+		}
+		if count > 0 {
+			count--
 		}
 	}
 }
